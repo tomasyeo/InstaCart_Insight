4cb988d614782d98be25892a3763bbe6c79c430e
tomas <ky2357@cumc.columbia.edu>
2019-04-03 11:04:58 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 #
 # Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python
 #
-python ./src/purchase_analytics.py -t 3 ./input/products.csv ./input/order_products.csv ./output/report.csv
+python3 ./src/purchase_analytics.py -t 3 ./input/products.csv ./input/order_products.csv ./output/report.csv
